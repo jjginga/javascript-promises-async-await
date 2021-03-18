67ab6710cc524b320c19b9825affb91b00009186
Joel José Ginga <jjginga@gmail.com>
2021-03-18 00:04:15 +0000

    third part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export function fetchMovies(){
+import {fetchWithTimeout} from './services';
 
+const movies = require('./data/movies.json');
+
+export function fetchMovies(){
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+const moviePromise = fetchMovies();
+
+moviePromise.then(function resolve(results){console.log(results)});
