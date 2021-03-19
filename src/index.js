5ef688cb337a0be72ade5165bde63ad158b9e6f6
Joel José Ginga <jjginga@gmail.com>
2021-03-19 23:20:51 +0000

    third part

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import {fetchWithTimeout} from './services';
+import {fetchWithTimeout, fetchMovies, fetchBooks} from './services';
 
-const movies = require('./data/movies.json');
-
-export function fetchMovies(){
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies(){
+    return Promise.all([fetchBooks(), fetchMovies()])
+                  .then(([books, movies]) => ({
+                        books,
+                        movies
+                  }))
+                  .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
+function getBooksOrMovies(){
+    return Promise.race([fetchBooks(), fetchMovies()])
+                  .then(results => results)
+                  .catch(error => console.log("Error waiting for the promise race", error));
+}
 
-moviePromise.then(function resolve(results){console.log(results)});
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(function resolve(results){console.log('getBooksAndMoviesPromise', results)});
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+//getBooksOrMoviesPromise.then(function resolve(results){console.log('getBooksOrMoviesPromise', results)});
